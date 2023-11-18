d8650e7b0819f0998814b603e8b580c36daefeaa
thimmie52 <67242050+thimmie52@users.noreply.github.com>
2023-11-18 12:17:15 +0100

    Update code_test_case.py

--- code_test_case/code_test_case.py
+++ code_test_case/code_test_case.py
@@ -1,240 +1,114 @@
-from enum import Enum
-from openai import OpenAI
+!pip install openai
+
+import openai
 import re
-import ast
-import textwrap
-from colorama import Fore, Style, init as init_colorama
-import dotenv
-import os
+
+from openai import OpenAI
 
 
-class GPTClient:
-    """
-    A class for interacting with the GPT API to generate coding challenge problem statements, test cases, and solutions.
+client = OpenAI(
+    api_key="",
+)
 
-    Attributes:
-        model (str): The name of the GPT model to use for generating responses.
-    """
-    def __init__(self):
-        """
-        Initializes the GPTClient object with the specified model name and API key.
+def run_code(result):
+  pattern = re.compile(r'def\s+(\w+)\s*\(.*?\):\s*\n(.*?)(?=\s*```)', re.DOTALL)
 
-        Args:
-            model (str): The name of the GPT model to use for generating responses.
-        """
-        init_colorama()
-        dotenv.load_dotenv()
-        self.model = "gpt-3.5-turbo"
-        print("Model attribute set successfully.")
-        # model_name = os.getenv("OPENAI_CHAT_MODEL")
+  # Find the match
+  match = pattern.search(result)
 
-        # if model_name:
-        #     self.model = model_name
-        # else:
-        #     self._print_formatted_error("Cannot find the model name!")
 
-        api_key = os.getenv("API_KEY")
+  expected_output = match.group(2).strip()
 
-        if api_key:
-            self.client = OpenAI(api_key=api_key)
-        else:
-            self._print_formatted_error("Cannot find the OpenAI API key!")
+  pattern = re.compile(r'^[ \t]+', re.MULTILINE)
 
-    def generate_problem_statement(self, ):
-        """
-        Generates a coding challenge problem statement with description, input format, output format, constraints,
-        and example inputs and outputs.
+  # Replace indents with an empty string
+  results = re.sub(pattern, '', expected_output)
+  exec(results)
 
-        Returns:
-            tuple: A tuple containing the generated problem statement and the corresponding test cases.
-        """
-        # Call GPT API to generate a problem statement for a coding challenge
-        response = self.client.chat.completions.create(
-            model=self.model,
-            messages=[
-                {
-                    "role": "user", "content": "Generate a coding challenge problem statement with description, "
-                                               "input format, output format, constraints, and example inputs and "
-                                               "outputs."
-                },
-                {
-                    "role": "assistant",
-                    "content": "Generate a set of input/output examples with inputs and outputs that can be parsed "
-                               "using ast.literal_eval() in Python. The input/output format must adhere to the "
-                               "following structure:\n\n1. In: [input_1]\nOut: [output_1]\n2. In: [input_2]\nOut: ["
-                               "output_2]\n...\n\nPlease ensure that 'In:' and 'Out:' keywords are only used to "
-                               "denote the input and output examples in the response. Additionally, provide examples "
-                               "with a variety of Python literals, including tuples, dictionaries, lists, booleans, "
-                               "and numerical values. Verify that the generated statement contains the 'In:' and "
-                               "'Out:' test cases before returning the result."
-                }
-            ],
-            temperature=1,
-            max_tokens=400,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0,
-        )
+def code_test_case(code, run = False):
+  """
+    This is a function that takes in a text of a function as an input and then returns test cases of that function.
 
-        print(response)
-        
-        # Extract the generated problem statement from the response
-        problem_statement = response.choices[0].message.content.strip()
-        self._print_formatted_problem(problem_statement)
+    --------------------------------------------------------------------------------------------------------------
 
-        # Parse the problem statement to extract test cases
-        test_cases = self.parse_test_cases(problem_statement)
-        self._print_formatted_test_cases(test_cases)
+    Parameters:
 
-        return problem_statement, test_cases
+    Code - Str. This is the code you are generating a test case for. Insert as a text.
+    run - Bool. If True then the function runs the test cases to see if all instances are true for all use cases, If false, then the output is the test case. Dafault is False
 
-    def update_problem_statement(self, problem_statement, feedback):
-        """
-        Generates an updated coding challenge problem statement based on the provided feedback.
 
-        Args:
-            problem_statement (str): The initial problem statement to improve.
-            feedback (str): Feedback indicating the issue with the initial problem statement.
+    ---------------------------------------------------------------------------------------------------------------
 
-        Returns:
-            tuple: A tuple containing the updated problem statement and the corresponding test cases.
-        """
-        # Call GPT API to generate a problem statement for a coding challenge
-        response = self.client.chat.completions.create(
-            model=self.model,
-            messages=[
-                {
-                    "role": "user", "content": f"{problem_statement}\nThe problem statement above does not seem to "
-                                               f"contain the correct input / output. This is the possible issue: \n "
-                                               f"{feedback}. Please review the test cases and correct and regenerate "
-                                               f"the statement."
-                },
-                {
-                    "role": "assistant",
-                    "content": "The input/output format must adhere to the "
-                               "following structure:\n\n1. In: [input_1]\nOut: [output_1]\n2. In: [input_2]\nOut: ["
-                               "output_2]\n...\n\nPlease ensure that 'In:' and 'Out:' keywords are only used to "
-                               "denote the input and output examples in the response."
-                }
-            ],
-            temperature=0.4,
-            max_tokens=400,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0,
-        )
-        # Extract the generated problem statement from the response
-        problem_statement = response.choices[0].message.content.strip()
-        self._print_formatted_problem(f"UPDATE:\n{problem_statement}")
+    Example:
 
-        # Parse the problem statement to extract test cases
-        test_cases = self.parse_test_cases(problem_statement)
-        self._print_formatted_test_cases(test_cases)
+    Input.
+      code_1 = "def sum_of_numbers(numbers):
+                  return sum(numbers)"
 
-        return problem_statement, test_cases
+      code_test_case(code_1)
 
-    def parse_test_cases(self, problem_statement):
-        """
-        Extracts and parses test cases from the provided problem statement.
+    Output.
+      *** def test_sum_of_numbers():
+            # Test Case 1: Sum of positive numbers
+            numbers_list = [1, 2, 3, 4, 5]
+            assert sum_of_numbers(numbers_list) == 15
 
-        Args:
-            problem_statement (str): The problem statement containing test cases.
+            # Test Case 2: Sum of negative numbers
+            numbers_list = [-1, -2, -3, -4, -5]
+            assert sum_of_numbers(numbers_list) == -15
 
-        Returns:
-            list: A list of tuples containing the extracted input and expected output values for each test case.
-        """
-        # Use regular expressions to extract test case information
-        input_pattern = re.compile(r"In:\s*(.+)")
-        output_pattern = re.compile(r"Out:\s*(.+)")
+            # Test Case 3: Sum of a mix of positive and negative numbers
+            numbers_list = [1, -2, 3, -4, 5]
+            assert sum_of_numbers(numbers_list) == 3
 
-        input_matches = input_pattern.findall(problem_statement)
-        output_matches = output_pattern.findall(problem_statement)
+            # Test Case 4: Sum of an empty list (should be 0)
+            assert sum_of_numbers([]) == 0
 
-        # Combine inputs and outputs into test cases
-        test_cases = list(zip(input_matches, output_matches))
+            # Test Case 5: Sum of a single number
+            assert sum_of_numbers([42]) == 42 ***
 
-        # Process inputs and outputs, e.g., convert to tuples, lists, or other data structures
-        processed_test_cases = []
-        for case_input, expected_output in test_cases:
-            # Process input and output based on the problem statement format
-            processed_input = ast.literal_eval(case_input)
-            processed_output = ast.literal_eval(expected_output)
 
-            processed_test_cases.append((processed_input, processed_output))
 
-        return processed_test_cases
 
-    def generate_algorithm(self, problem_statement):
-        """
-        Generates a Python function to solve the problem described in the provided problem statement.
+  """
+  prompt = '''I want to find about 20 effective testcases for my function. My input is something like
+              def sum_of_numbers(numbers):
+                return sum(numbers)
+                I expect an output of effective test cases for my function. An output like:
 
-        Args:
-            problem_statement (str): The problem statement describing the challenge.
+                def test_sum_of_numbers():
+            # Test Case 1: Sum of positive numbers
+            numbers_list = [1, 2, 3, 4, 5]
+            assert sum_of_numbers(numbers_list) == 15, "Positive numbers"
 
-        Returns:
-            str: The generated Python function code to solve the problem.
-        """
-        # Call GPT API with the problem statement to generate potential solution code
-        response = self.client.chat.completions.create(
-            model=self.model,
-            messages=[{
-                "role": "user",
-                "content": f"{problem_statement}\n\nPlease provide a Python function to solve the above problem. The "
-                           "generated code should be complied without any additional modification. So, no comments, "
-                           "markdown, code fence, etc in the response."
-            }],
-            temperature=0.3,
-            max_tokens=350,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0,
-        )
+            # Test Case 2: Sum of negative numbers
+            numbers_list = [-1, -2, -3, -4, -5]
+            assert sum_of_numbers(numbers_list) == -15, "Negative numbers"
 
-        # Extract the generated solution code from the response
-        solution_code = response.choices[0].message.content.strip()
-        self._print_formatted_solution(solution_code)
-        return solution_code
+            # Test Case 3: Sum of a mix of positive and negative numbers
+            numbers_list = [1, -2, 3, -4, 5]
+            assert sum_of_numbers(numbers_list) == 3, "Mixture of Positive and negative"
 
-    def _print_formatted_problem(self, problem_statement):
-        """Prints the provided problem statement in a visually appealing and formatted manner.
+            # Test Case 4: Sum of an empty list (should be 0)
+            assert sum_of_numbers([]) == 0, "Empty list"
 
-        Args:
-        problem_statement (str): The problem statement to display in a formatted way.
-        """
-        separator = "=" * 80
-        print(Fore.CYAN + separator)
-        print(Fore.GREEN + "Problem Statement:\n")
-        print(Fore.RESET + textwrap.fill(problem_statement, width=80))
+            # Test Case 5: Sum of a single number
+            assert sum_of_numbers([42]) == 42., "Single number"
 
-    def _print_formatted_test_cases(self, test_cases):
-        """Prints the provided test cases in a clear and organized manner.
+            Meaning no additional text from you, just an output of the function to test the cases and call the funcion at the end. NO EXTRA TEXT, Make sure the assertion is like `assert sum_of_numbers(numbers_list) == 3, "Mixture of Positive and negative"`
+            Using this data, this is my input:'''
 
-        Args:
-            test_cases (list): A list of tuples containing input and output values for each test case.
-        """
-        for index, (input_value, output_value) in enumerate(test_cases):
-            print(Fore.LIGHTMAGENTA_EX + f"Test Case {index + 1}: Input: {input_value} Expected: {output_value}")
-        print(Fore.RESET)
-
-    def _print_formatted_solution(self, solution_code):
-        """Presents the provided solution code in a highlighted and well-structured format.
-
-        Args:
-            solution_code (str): The solution code to display in an enhanced manner.
-        """
-        separator = "=" * 80
-        print(Fore.CYAN + separator)
-        print(Fore.GREEN + "Solution Code:\n")
-        print(Fore.LIGHTGREEN_EX + solution_code)
-        print(Fore.CYAN + separator)
-        print(Style.RESET_ALL)  # Reset color to default
-
-    def _print_formatted_error(self, error):
-        """Displays the provided error message in a prominent and attention-grabbing manner.
-
-        Args:
-            error (str): The error message to present in a visually distinctive way.
-        """
-        print(Fore.RED + error)
-        print(Style.RESET_ALL)
-
+  response = client.chat.completions.create(
+                messages=[
+                        {
+                            "role": "user",
+                            "content": f"{prompt} {code}",
+                        }
+                    ],
+                    model="gpt-3.5-turbo",
+                        )
+  answer = response.choices[0].message.content
+  if run == False:
+    print(answer)
+  else:
+    run_code(f"{code} \n {answer}")
